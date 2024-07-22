35c5e0abb09bc0ebcbecfdab636d4e0b80221edf
GuviGitHub <samuelguvi.fdk@gmail.com>
2024-07-22 11:12:43 -0600

    primer subida version 1

---
+++ include/navbar.php
@@ -0,0 +1,38 @@
+<header class="fixed-top">
+        <div class="linea-nav"></div>
+        <nav class="navbar container-fluid navbar-expand-lg  bg-white">
+            <div class="container d-flex flex-xl-column">
+                <div class="mb-3">
+                    <a class="navbar-brand" href="index.html">
+                        <img src="src/img/main/logo.png" alt="Logo coclo" class="logo-nav">
+                    </a>
+                </div>
+
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse " id="navbarTogglerDemo01">
+                    <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="nosotros.html">Nosotros</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Cocina</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Closets</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Muebles de baño</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Contacto</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
